0d4d3f9e32c67064d31b547a9f507a8f8bcbbf89
Joaopcamposs <joaopcamposs@outlook.com>
2023-12-16 16:04:44 -0300

    Implementando padrão unidade de trabalho

--- src/allocation/entrypoints/fast_api.py
+++ src/allocation/entrypoints/fast_api.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 from src.allocation.adapters.orm import metadata
 from src.allocation.domain import model
-from src.allocation.adapters import orm, repository
+from src.allocation.adapters import orm
 from src.allocation.entrypoints.schemas import CreateAllocation, CreateBatch
-from src.allocation.service_layer import services
+from src.allocation.service_layer import services, unit_of_work
 from decouple import config
 
 
@@ -37,36 +37,32 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-@app.post("/allocate")
-def allocate_endpoint(
-    new_allocate: CreateAllocation,
-    session: Session = Depends(get_db),
-):
-    repo = repository.SqlAlchemyRepository(session)
-
-    try:
-        batchref = services.allocate(
-            new_allocate.orderid, new_allocate.sku, new_allocate.qty, repo, session
-        )
-    except (model.OutOfStock, services.InvalidSku) as e:
-        raise HTTPException(detail=f"error: {e}", status_code=400)
-
-    return JSONResponse(status_code=201, content={"batchref": batchref})
-
-
 @app.post("/add_batch")
 def add_batch(
     new_batch: CreateBatch,
-    session: Session = Depends(get_db),
 ):
-    repo = repository.SqlAlchemyRepository(session)
-
     services.add_batch(
         new_batch.ref,
         new_batch.sku,
         new_batch.qty,
         new_batch.eta,
-        repo,
-        session,
+        unit_of_work.SqlAlchemyUnitOfWork(),
     )
     return JSONResponse(status_code=201, content="OK")
+
+
+@app.post("/allocate")
+def allocate_endpoint(
+    new_allocate: CreateAllocation,
+):
+    try:
+        batchref = services.allocate(
+            new_allocate.orderid,
+            new_allocate.sku,
+            new_allocate.qty,
+            unit_of_work.SqlAlchemyUnitOfWork(),
+        )
+    except (model.OutOfStock, services.InvalidSku) as e:
+        raise HTTPException(detail=f"error: {e}", status_code=400)
+
+    return JSONResponse(status_code=201, content={"batchref": batchref})
